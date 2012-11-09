be372843de7cc271269c05c5e6a1de4a76e7e12a
Yussuf Khalil <dev@pp3345.de>
2012-11-10 00:36:25 +0100

    Added .longDefine
    #.longDefine 'abc'
    $x = 7;
    $a = 0;
    echo $x . $a;
    #.endLongDefine
    The above code will create a constant with all data between .longDefine
    and .endLongDefine

---
+++ instructionHandlers/endLongDefine.php
@@ -0,0 +1,40 @@
+<?php
+
+	/****************************************************************/
+	/* Moody                                                        */
+	/* endLongDefine.php                 					        */
+	/* 2012 Yussuf Khalil                                           */
+	/****************************************************************/
+	
+	namespace Moody\InstructionHandlers {
+		use Moody\InstructionHandlerWithRegister;
+		use Moody\Token;
+		use Moody\TokenHandlers\InstructionProcessor;
+		use Moody\TokenVM;
+		use Moody\InstructionProcessorException;
+		use Moody\MultiTokenInstruction;
+		use Moody\ConstantContainer;
+	
+		class EndLongDefineHandler implements InstructionHandlerWithRegister {
+			private static $instance = null;
+				
+			private function __construct() {
+				InstructionProcessor::getInstance()->registerHandler('endlongdefine', $this);
+			}
+				
+			public static function getInstance() {
+				if(!self::$instance)
+					self::$instance = new self;
+				return self::$instance;
+			}
+				
+			public function execute(Token $token, $instructionName, InstructionProcessor $processor, TokenVM $vm = null, $executionType = 0) {
+				return TokenVM::DELETE_TOKEN;
+			}
+				
+			public function register(Token $token, $instructionName, InstructionProcessor $processor, TokenVM $vm) {
+				MultiTokenInstruction::setEndToken($token, 'longDefine');
+			}
+		}
+	}
+?>
--- instructionHandlers/longDefine.php
+++ instructionHandlers/longDefine.php
@@ -7,20 +7,19 @@
 	/****************************************************************/
 	
 	namespace Moody\InstructionHandlers {
-		use Moody\InstructionHandler;
+		use Moody\InstructionHandlerWithRegister;
 		use Moody\Token;
 		use Moody\TokenHandlers\InstructionProcessor;
 		use Moody\TokenVM;
 		use Moody\InstructionProcessorException;
+		use Moody\MultiTokenInstruction;
+		use Moody\ConstantContainer;
 	
-		class LongDefineHandler implements InstructionHandler {
+		class LongDefineHandler implements InstructionHandlerWithRegister {
 			private static $instance = null;
 			
 			private function __construct() {
-				InstructionProcessor::getInstance()->registerHandler('define', $this);
-				InstructionProcessor::getInstance()->registerHandler('def', $this);
-				InstructionProcessor::getInstance()->registerHandler('d', $this);
-				InstructionProcessor::getInstance()->registerDefaultHandler($this);
+				InstructionProcessor::getInstance()->registerHandler('longdefine', $this);
 			}
 			
 			public static function getInstance() {
@@ -30,29 +29,35 @@
 			}
 			
 			public function execute(Token $token, $instructionName, InstructionProcessor $processor, TokenVM $vm = null, $executionType = 0) {
+				$args = $processor->parseArguments($token, $instructionName, 's');
 				
-			}
-		}
-	}
-	
-	namespace Moody\TokenHandlers {
-		use Moody\TokenHandler;
-		use Moody\Token;
-		use Moody\TokenHandlers\InstructionProcessor;
-		use Moody\TokenVM;
-		use Moody\InstructionProcessorException;
-		
-		class LongDefineHandler implements InstructionHandler {
-			private static $instance = null;
-				
-			public static function getInstance() {
-				if(!self::$instance)
-					self::$instance = new self;
-				return self::$instance;
+				foreach(MultiTokenInstruction::getAll('longDefine') as $instruction) {
+					if($instruction->getToken() == $token) {
+						$endToken = $instruction->getEndToken();
+						
+						if(!($endToken instanceof Token))
+							throw new InstructionProcessorException('Invalid end token for ' . $instructionName . ' - Probably you forgot an endLongDefine?', $token);
+						
+						$tokens = $vm->getTokenArray();
+						
+						$data = "";
+						
+						while($token = current($tokens)) {
+							if($token == $endToken) {
+								ConstantContainer::define($args[0], $data);
+								$vm->jump($token);
+								return TokenVM::JUMP | TokenVM::DELETE_TOKEN;
+							}
+							
+							$data .= $token->content;
+							next($tokens);
+						}
+					}
+				}
 			}
 			
-			public function execute(Token $token, TokenVM $vm) {
-				
+			public function register(Token $token, $instructionName, InstructionProcessor $processor, TokenVM $vm) {
+				new MultiTokenInstruction($token, 'longDefine');
 			}
 		}
 	}
