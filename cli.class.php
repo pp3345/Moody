77492b6b2c24347fae61fbf4aa0c78b0681f1159
Yussuf Khalil <dev@pp3345.de>
2012-11-23 13:54:08 +0100

    Completely new CLI interface
    Still not completed, though
    
    New features:
    - Support script execution by CLI argument (for example "php
    cli.class.php myCode.php myCode.cphp" or "php cli.class.php fun.mdy")
    - Support .mdy and .moody for Moody-only source code
    - Benchmark mode ("php cli.class.php --benchmark fun.mdy")
    
    More features coming soon.

--- cli.class.php
+++ cli.class.php
@@ -2,7 +2,7 @@
 
 	/****************************************************************/
 	/* Moody                                                        */
-	/* moodyException.class.php                                     */
+	/* cli.class.php                                     			*/
 	/* 2012 Yussuf Khalil                                           */
 	/****************************************************************/
 	
@@ -12,46 +12,162 @@
 	 * Command line interface for Moody
 	 */
 	class CLI  {
-		public function __construct() {
-			enter:
-			echo 'Please enter file to process: ';
+		private $argv = array();
+		private $args = array();
+		private $inExecute = false;
+		
+		public function main($argv) {
+			$this->argv = $argv;
+			unset($this->argv[0], $argv);
 			
-			$fileName = str_replace(array("\r\n", "\n", "\r"), "", fread(STDIN, 128));
-			if(!file_exists($fileName)) {
-				echo "\r\nInvalid filename\r\n";
-				goto enter;
+			set_error_handler(array($this, 'errorHandler'));
+			
+			entry:
+			
+			/* Load moody.cphp */
+			if(!file_exists('moody.cphp')) {
+				if(file_exists('build.php') && is_readable('build.php')) {
+					echo "moody.cphp not found. Build from source now? (y/n) ";
+					if(strtolower(fread(\STDIN, 1)) != 'y') {
+						echo "Aborting." . \PHP_EOL;
+						exit;
+					} else {
+						require_once 'build.php';
+						
+						/* Do not load moody.cphp as build.php already has included the Moody sources */
+						goto loaded;
+					}
+				} else {
+					echo "moody.cphp not found." . \PHP_EOL;
+					exit;
+				}
 			}
 			
-			ConstantContainer::initialize();
-			$tokenArray = Token::tokenize(file_get_contents($fileName), $fileName);
-			$vm = new TokenVM();
-			try {
-				$tokenArray = $vm->execute($tokenArray);
-			} catch(\Exception $e) {
-				echo (string) $e . "\r\n";
+			if(!is_readable('moody.cphp')) {
+				echo "moody.cphp exists but is not readable." . \PHP_EOL;
 				exit;
 			}
 			
-			$newCode = "";
+			require_once 'moody.cphp';
 			
-			foreach($tokenArray as $token) {
-				$newCode .= $token->content;
+			loaded:
+			
+			/* Load token handlers */
+			foreach(get_declared_classes() as $class) {
+				if(in_array('Moody\TokenHandler', class_implements($class)))
+					$class::getInstance();
 			}
 			
-			echo "\r\nNew code:\r\n";
-			echo $newCode;
-			echo "\r\n";
+			/* Parse arguments */
+			foreach($this->argv as $arg) {
+				if(substr($arg, 0, 1) != '-')  {
+					if(isset($executeFile))
+						$destinationFile = $arg;
+					else
+						$executeFile = $arg;
+					break;
+				}
+			}
+			
+			$this->args = getopt("", array('benchmark', 'silent'));
+			
+			if(isset($executeFile)) {
+				$source = $this->executeFile($executeFile);
+				
+				if($source === false)
+					exit;
+				
+				if(isset($destinationFile)) {
+					if(!file_put_contents($destinationFile, $source))
+						echo "Failed to put new source into destination file." . \PHP_EOL;
+				}
+				
+				exit;
+			}
+			
+			echo "Moody CLI Interpreter v" . MOODY_VERSION . \PHP_EOL;
+			echo "2012 Yussuf Khalil" . \PHP_EOL;
+		}
+		
+		public function executeFile($fileName) {
+			if(!file_exists($fileName)) {
+				echo "File does not exist." . \PHP_EOL;
+				return false;
+			}
+			
+			if(!is_readable($fileName)) {
+				echo "File is not readable." . \PHP_EOL;
+				return false;
+			}
+			
+			$file = file_get_contents($fileName);
+			
+			if(substr($fileName, -3) == 'mdy' || substr($fileName, -5) == "moody") {
+				/* Satisfy the tokenizer */
+				$file = '<?php ' . $file . ' ?>';
+			}
+			
+			return $this->executeSource($file, $fileName);
+		}
+		
+		public function executeSource($source, $origin = "Unknown") {
+			$source = $this->executeScript(Token::tokenize($source, $origin));
+			
+			if(is_array($source)) {
+				$sourceString = "";
+				
+				foreach($source as $token)
+					$sourceString .= $token->content;
+				
+				return $sourceString;
+			}
+			
+			return $source;
+		}
+		
+		public function executeScript($tokenArray) {
+			try {
+				ConstantContainer::initialize();
+				
+				$vm = new TokenVM;
+				
+				$this->inExecute = true;
+				
+				if(isset($this->args['benchmark'])) {
+					$timeStart = microtime(true);
+				}
+				
+				$tokenArray = $vm->execute($tokenArray);
+				
+				if(isset($this->args['benchmark'])) {
+					echo "Script execution took " . ((microtime(true) - $timeStart) * 1000) . " ms." . \PHP_EOL;
+				}
+				
+				$this->inExecute = false;
+				return $tokenArray;
+			} catch(\Exception $exception) {
+				if(isset($this->args['benchmark']))
+					$executionTime = (microtime(true) - $timeStart) * 1000;
+				
+				echo (string) $exception . \PHP_EOL;
+				
+				if(isset($this->args['benchmark'])) {
+					echo "Script execution took " . $executionTime . " ms." . \PHP_EOL;
+				}
+				
+				$this->inExecute = false;
+				return false;
+			}
+		}
+		
+		public function errorHandler($errType, $errStr, $errFile, $errLine) {
+			if($this->inExecute)
+				throw new MoodyException($errStr, $errType);
+			
+			throw new \ErrorException($errStr, 0, $errType, $errFile, $errLine);
 		}
 	}
 	
-	require_once 'moody.cphp';
-	
-	foreach(get_declared_classes() as $class) {
-		if(in_array('Moody\TokenHandler', class_implements($class)) || in_array('Moody\InstructionHandler', class_implements($class)))
-			$class::getInstance();
-	}
-	
-	ConstantContainer::initialize();
-	
-	new CLI;
+	$cli = new CLI;
+	$cli->main($argv);
 ?>
