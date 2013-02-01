d17d38be2a94c67d7a6e5b2595966bd84235ac4b
Yussuf Khalil <dev@pp3345.net>
2013-02-01 21:36:34 +0100

    Allow setting configuration callbacks

--- configuration.class.php
+++ configuration.class.php
@@ -5,28 +5,43 @@
 	/* configuration.class.php                                      */
 	/* 2012 Yussuf Khalil                                           */
 	/****************************************************************/
-	
+
 	namespace Moody {
-	
+
 	class Configuration {
 		private static $configuration = array();
-		
+		private static $callbacks = array();
+
 		public static function load($string) {
-			
+
 		}
-		
-		public static function set($path, $value) {
-			return self::$configuration[strtolower($path)] = $value;
+
+		public static function set($path, $value, TokenVM $tokenVM = null) {
+			$path = strtolower($path);
+			if(isset(self::$callbacks[$path])) {
+				call_user_func(self::$callbacks[$path], $value, $tokenVM);
+			}
+			return self::$configuration[$path] = $value;
 		}
-		
+
 		public static function get($path, $defaultValue = null) {
 			$path = strtolower($path);
-			
+
 			if(isset(self::$configuration[$path]))
 				return self::$configuration[$path];
 			return $defaultValue;
 		}
+
+		public static function registerCallback($path, $defaultValue, $callback, $invoke = true) {
+			if(!is_callable($callback)) {
+				throw new MoodyException('Bad configuration callback');
+			}
+			self::$callbacks[strtolower($path)] = $callback;
+			if($invoke) {
+				$callback(isset(self::$configuration[$path]) ? self::$configuration[$path] : $defaultValue);
+			}
+		}
 	}
-	
+
 	}
 ?>
